5c38c37d4c11ac784f1edcbc5b5035de3609fe03
usmonovAbdilaziz <abdilazizusmonov993@gmail.com>
2025-10-30 12:38:09 +0500

    collect onse file

--- app.ts
+++ app.ts
@@ -1,7 +1,9 @@
 import { createServer, IncomingMessage } from "http";
-import { postData } from "./src/post.data";
-import { bodyData } from "./src/collect.body";
-import { getData } from "./src/get.data";
+import { config } from "dotenv";
+config();
+
+const url = String(process.env.urlWEB); // Target API endpoint for POST request
+const myUrl = String(process.env.url); // Webhook callback URL
 
 let success = false;
 let parts = { part1: "", part2: "" };
@@ -31,7 +33,49 @@ const server = createServer(async (req: IncomingMessage, res: any) => {
     console.log("Not found");
   }
 });
+//function postData
+async function postData() {
+  try {
+    // Send POST request with JSON body
+    const data = await fetch(url, {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify({ msg: "Hello world", url: myUrl }),
+    });
 
+    // Parse the JSON response from the server
+    const res = await data.json();
+
+    // Return the response
+    return res;
+  } catch (error) {
+    // Handle and log any errors that occur during the request
+    console.log("Error occurred while sending POST request");
+  }
+}
+//collect chunk
+async function bodyData(req: IncomingMessage): Promise<string> {
+  return new Promise((res, rej) => {
+    let body = "";
+
+    // Collect data chunks from the request
+    req.on("data", (chunk: Buffer) => {
+      body += chunk.toString();
+    });
+
+    // When all data has been received, resolve the promise
+    req.on("end", () => {
+      res(body);
+    });
+
+    // Handle any possible errors during data reading
+    req.on("error", (err: Object) => {
+      rej(err);
+    });
+  });
+}
 // Immediately execute an async function to send the initial POST request
 (async function () {
   try {
@@ -42,7 +86,17 @@ const server = createServer(async (req: IncomingMessage, res: any) => {
     console.log("Error while getting part1:", error);
   }
 })();
-
+//Request get
+async function getData(method: String) {
+  try {
+    const url = `${String(process.env.urlWEB)}?code=${method}`;
+    const data = await fetch(url);
+    const res = data.json();
+    return res;
+  } catch (error) {
+    console.log("Get qilishda xatolilk", error);
+  }
+}
 // Check every second if both parts are received
 const timer = setInterval(() => {
   if (success) {
